022e9db0ad1084adff15b5393d50ef149f0b05a1
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-01-13 06:48:22 -0600

    created trunk repository.
    git-svn-id: http://svn.symfony-project.com/plugins/pmModelUtilsPlugin/trunk@31821 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- cache/frontend/dev/modules/autoArticle/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="4">
-  <?php echo __('%%title%% - %%content%% - %%status%% - %%id%%', array('%%title%%' => $article->getTitle(), '%%content%%' => $article->getContent(), '%%status%%' => $article->getStatus(), '%%id%%' => link_to($article->getId(), 'article_edit', $article)), 'messages') ?>
-</td>
